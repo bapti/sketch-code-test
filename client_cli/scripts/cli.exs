ec987aae8cf326b7c018d0c58577ea819f72a097
Neil <neil.crawford@gmail.com>
2022-04-15 11:12:30 +0100

    Formatting output of CLI to make clearer

--- client_cli/scripts/cli.exs
+++ client_cli/scripts/cli.exs
@@ -1,8 +1,11 @@
 {[canvas: canvas_id], _, _} = OptionParser.parse(System.argv(), strict: [canvas: :integer])
-IO.inspect("Loading Canvas #{canvas_id}")
+IO.puts("\n\nCanvas #{canvas_id}")
+IO.puts("--------------------------------------------")
 
 %{body: body} = HTTPoison.get!("http://localhost:4000/api/render/#{canvas_id}")
 
 %{"data" => data} = Jason.decode!(body)
 
 for row <- data, do: IO.puts(row)
+
+IO.puts("--------------------------------------------\n\n")
